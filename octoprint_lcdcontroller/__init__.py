2630aa08646d88b4bcac4b836a7e4d1b5f3b8086
chickenchuck040 <thollaba@robostorm.org>
2015-08-25 16:00:58 -0400

    Main screen works

--- octoprint_lcdcontroller/__init__.py
+++ octoprint_lcdcontroller/__init__.py
@@ -3,15 +3,18 @@ from __future__ import absolute_import
 
 import octoprint.plugin
 import serial
+from .lcd import Lcd
 
 serialPort = "/dev/ttyAMA0"
 
-lcd = serial.Serial(serialPort)
+#lcd = serial.Serial(serialPort)
+
+lcd = Lcd()
 
 class LcdController(octoprint.plugin.StartupPlugin):
 	def on_after_startup(self):
 		self._logger.info("Lcd Controller starting")
-		lcd.write("?f")
-		lcd.write("Octoprint Started")
+		lcd.clear()
+		lcd.write("OctoPrint Started")
 
-__plugin_implementation__ = LcdCOntroller()
+__plugin_implementation__ = LcdController()
